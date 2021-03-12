733ea170ca0610fa7285ffba3ea9d3a4c7a13293
bhaworth <ben@benhaworth.com>
2021-03-12 16:50:47 +0000

    bootstrap reorder

--- userdata/bootstrap.sh
+++ userdata/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-p /etc/motd /etc/motd.bkp
+cp /etc/motd /etc/motd.bkp
 cat << EOF > /etc/motd
  
 I have been modified by cloud-init at $(date)
@@ -8,7 +8,24 @@ Ben Haworth
 
 EOF
 
+sleep 30
+
 sudo apt update -y
+
+# Partition and Format Block Volumes
+
+echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdb >> /tmp/bootstrap.log
+echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdc >> /tmp/bootstrap.log
+
+sudo mkfs -t ext3 /dev/oracleoci/oraclevdb1 >> /tmp/boostrap.log
+sudo mkfs -t ext3 /dev/oracleoci/oraclevdc1 >> /tmp/boostrap.log
+
+# Mount the volumes
+
+sudo mkdir /data /work
+
+sudo mount /dev/oracleoci/oraclevdb1 /data
+sudo mount /dev/oracleoci/oraclevdc1 /work
 
 # Install OCI-CLI
 
@@ -25,23 +42,5 @@ sudo apt-get install nfs-server -y
 # Add NFS to iptables
 
 ####### TODO
-
-# Partition and Format Block Volumes
-# No Sleep before this assuming CLI install and NFS take > 30s.
-# PV Block vols are not available immediately
-
-echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdb >> /tmp/bootstrap.log
-echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdc >> /tmp/bootstrap.log
-
-sudo mkfs -t ext3 /dev/oracleoci/oraclevdb1 >> /tmp/boostrap.log
-sudo mkfs -t ext3 /dev/oracleoci/oraclevdc1 >> /tmp/boostrap.log
-
-# Mount the volumes
-
-sudo mkdir /data /work
-
-sudo mount /dev/oracleoci/oraclevdb1 /data
-sudo mount /dev/oracleoci/oraclevdc1 /work
-
 
 
