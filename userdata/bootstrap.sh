03f7103014c1a27fe650d38d118b4a179120b638
bhaworth <ben@benhaworth.com>
2021-03-12 14:14:21 +0000

    add oci cli install to bootstrap

--- userdata/bootstrap.sh
+++ userdata/bootstrap.sh
@@ -7,12 +7,13 @@ I have been modified by cloud-init at $(date)
 Ben Haworth
 EOF
 
-sleep 2m
+# Wait for devices to appear
+sleep 30s
 
 # Partition and Format Block Volumes
 
-echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdb >> /tmp/boostrap.log
-echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdc >> /tmp/boostrap.log
+echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdb >> /tmp/bootstrap.log
+echo 'type=83' | sudo sfdisk /dev/oracleoci/oraclevdc >> /tmp/bootstrap.log
 
 sudo mkfs -t ext3 /dev/oracleoci/oraclevdb1 >> /tmp/boostrap.log
 sudo mkfs -t ext3 /dev/oracleoci/oraclevdc1 >> /tmp/boostrap.log
@@ -26,3 +27,8 @@ sudo mount /dev/oracleoci/oraclevdc1 /work
 
 # Install OCI-CLI
 
+sudo su - ubuntu
+wget -O ociinstall_wget.sh https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh
+chmod 755 ociinstall_wget.sh
+./ociinstall.sh --accept-all-defaults
+exit
