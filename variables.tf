ed3f30b92c0fcd589e90435a84e2c39ce1417f01
bhaworth <ben@benhaworth.com>
2021-03-15 10:54:51 +0000

    schema

--- variables.tf
+++ variables.tf
@@ -2,7 +2,9 @@ variable "region" {}
 variable "compartment_ocid" {}
 variable "ssh_pub_key" {}
 variable "bastion_shape" {}
+variable "bastion_image" {}
 variable "headnode_shape" {}
+variable "headnode_image" {}
 variable "ad" {}
 variable "name_prefix" {}
 variable "env_name" {
