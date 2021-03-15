53c55dbb48c9850783358fc60eb3b9f200b8c049
bhaworth <ben@benhaworth.com>
2021-03-15 10:14:50 +0000

    schema.yaml ver1

--- variables.tf
+++ variables.tf
@@ -4,3 +4,8 @@ variable "compartment_ocid" {}
 variable "ssh_pub_key" {}
 variable "bastion_shape" {}
 variable "headnode_shape" {}
+variable "ad" {}
+variable "name_prefix" {}
+variable "env_name" {
+    default = "sp3"
+}
