564c55e577958c2fb6d8a7003848773967af908c
bhaworth <ben@benhaworth.com>
2021-03-17 20:34:16 +0000

    add policy for HN DG

--- iam.tf
+++ iam.tf
@@ -3,5 +3,15 @@ resource "oci_identity_dynamic_group" "HeadNode_DG" {
 
   description   = "Group for Head Node in deployment ${local.Sp3_deploy_id}"
   matching_rule = "Any {All {instance.id = ${local.Sp3Headnode_id}}}"
-  name          = "${local.Sp3_env_name}_HeadNode_Dynamic_Group"
+  name          = "${local.Sp3_env_name}_HeadNode"
+}
+
+resource "oci_identity_policy" "HeadNode_Policy" {
+  compartment_id = var.tenancy_ocid
+
+  description = "Policy for Head Node in deployment ${local.Sp3_deploy_id}"
+  statements = [
+    "Allow dynamic-group ${local.Sp3_env_name}_HeadNode to manage all-resources in compartment sandbox",
+  ]
+  name = "${local.Sp3_env_name}_HeadNode"
 }
