476bf8a4f5274689fa95be150da2b99e630b2469
bhaworth <ben@benhaworth.com>
2021-03-18 14:42:03 +0000

    comment re permissions

--- iam.tf
+++ iam.tf
@@ -10,6 +10,9 @@ resource "oci_identity_policy" "HeadNode_Policy" {
   compartment_id = local.Sp3_cid
 
   description = "Policy for Head Node in deployment ${local.Sp3_deploy_id}"
+  
+  # Need to know what the correct permissions required are  <<CHANGE_ME>>
+  
   statements = [
     "Allow dynamic-group ${oci_identity_dynamic_group.HeadNode_DG.name} to manage all-resources in compartment id ${local.Sp3_cid}",
   ]
