b3e104d05764aa1952d7ebab8144858da7de99af
bhaworth <ben@benhaworth.com>
2021-03-20 18:58:54 +0000

    Revert "handle comp_id"
    This reverts commit fc0aea1aa8536e6338b5941716a78164920dd8b4.

--- iam.tf
+++ iam.tf
@@ -29,7 +29,3 @@ resource "oci_identity_compartment" "sp3_child_comp" {
 locals { 
   Sp3_cid = oci_identity_compartment.sp3_child_comp.id 
 }
-
-output "sp3_child_compartment" {
-  value = local.Sp3_cid
-}
