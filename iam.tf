cb5ca461beddd6dde07f8ac0f53f54fef72360cf
bhaworth <ben@benhaworth.com>
2021-03-20 18:04:42 +0000

    new compartment for stack

--- iam.tf
+++ iam.tf
@@ -10,11 +10,22 @@ resource "oci_identity_policy" "HeadNode_Policy" {
   compartment_id = local.Sp3_cid
 
   description = "Policy for Head Node in deployment ${local.Sp3_deploy_id}"
-  
+
   # Need to know what the correct permissions required are  <<CHANGE_ME>>
-  
+
   statements = [
     "Allow dynamic-group ${oci_identity_dynamic_group.HeadNode_DG.name} to manage all-resources in compartment id ${local.Sp3_cid}",
   ]
   name = "${local.Sp3_env_name}_HeadNode"
+}
+
+resource "oci_identity_compartment" "sp3_child_comp" {
+  enable_delete  = true
+  compartment_id = var.compartment_id
+  description    = "Compartment for the SP3 Cluster with Deployment ID: ${local.Sp3_deploy_id}"
+  name           = "deployment_${local.Sp3_deploy_id}"
+}
+
+locals { 
+  Sp3_cid = oci_identity_compartment.sp3_child_comp.id 
 }
