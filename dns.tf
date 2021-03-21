2c694863949e0f07f051aabda8351dddd88e13fd
bhaworth <ben@benhaworth.com>
2021-03-21 19:41:44 +0000

    items fix

--- dns.tf
+++ dns.tf
@@ -1,12 +1,16 @@
 resource "oci_dns_rrset" "lb_a_record" {
-    count = var.create_dns ? 1 : 0
-    
+  count = var.create_dns ? 1 : 0
+
+  domain          = "${local.Sp3_env_name}.oci.sp3dev.ml"
+  rtype           = "A"
+  zone_name_or_id = local.Sp3dev_ml_dns_zone_id
+  #compartment_id = local.Sp3dev_ml_dns_comp_id
+  items {
     domain = "${local.Sp3_env_name}.oci.sp3dev.ml"
-    rtype = "A"
-    zone_name_or_id = local.Sp3dev_ml_dns_zone_id
-    #compartment_id = local.Sp3dev_ml_dns_comp_id
-    items {
-        rdata = "${local.Sp3_lb_public_ip}"
-        ttl = 30
-    }
+    rtype  = "A"
+    rdata  = local.Sp3_lb_public_ip
+    ttl    = 30
+
+  }
+
 }
