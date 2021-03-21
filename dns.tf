cec6fbc996b47144eddd33854928c3cbb4842c84
bhaworth <ben@benhaworth.com>
2021-03-21 19:24:47 +0000

    conditional DNS

--- dns.tf
+++ dns.tf
@@ -1,4 +1,6 @@
 resource "oci_dns_rrset" "lb_a_record" {
+    count = var.create_dns ? 1 : 0
+    
     domain = "${local.Sp3_env_name}.oci.sp3dev.ml"
     rtype = "A"
     zone_name_or_id = local.Sp3dev_ml_dns_zone_id
