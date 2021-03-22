71d2c14fb26e98c7d6926035ba96418c4c4834ad
bhaworth <ben@benhaworth.com>
2021-03-22 22:47:33 +0000

    instance_principal

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -2,14 +2,11 @@
 
 set -x
 
-# Set OCI CLI Auth Env Var
-
-OCI_CLI_AUTH=instance_principal
-
 # Pull the Private Key for GitLab access
 
 oci secrets secret-bundle get \
  --raw-output \
+ --auth instance_principal
  --secret-id ${Sp3_gitrepo_secret_id} \
  --query "data.\"secret-bundle-content\".content" | base64 --decode > /home/ubuntu/.ssh/gitlab_key
 
