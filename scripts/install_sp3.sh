ecf1e4bd3321ac9c31f1005dc245e21afab4ddf8
bhaworth <ben@benhaworth.com>
2021-03-22 20:20:37 +0000

    typo in key file name

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Pull the Private Key for GitLab access
 
 oci secrets secret-bundle get \
@@ -15,7 +17,7 @@ chmod 600 /home/ubuntu/.ssh/gitlab_key
 GIT_SSH_COMMAND='ssh -i /home/ubuntu/.ssh/gitlab_key -o StrictHostKeyChecking=no' git clone git@gitlab.com:MMMCloudPipeline/sp3.git
 
 ssh-keygen -t rsa -f /home/ubuntu/.ssh/self_id_rsa -q -P ""
-cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
+cat /home/ubuntu/.ssh/self_id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 sed -i '13s/.*/cd \/home\/ubuntu\/sp3/' /home/ubuntu/sp3/sp3doc/install-basic.bash
 ssh -i /home/ubuntu/.ssh/self_id_rsa -o StrictHostKeyChecking=no ubuntu@localhost bash /home/ubuntu/sp3/sp3doc/install-basic.bash
 ssh -i /home/ubuntu/.ssh/self_id_rsa -o StrictHostKeyChecking=no ubuntu@localhost bash /home/ubuntu/sp3/sp3doc/install-oci.sh
