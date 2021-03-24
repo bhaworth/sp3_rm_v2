e7a8f2d8cb45e16be709949cfcfa6d4d75ad9ee0
bhaworth <ben@benhaworth.com>
2021-03-24 09:44:17 +0000

    status messages in log

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -14,6 +14,7 @@ chmod 600 /home/ubuntu/.ssh/gitlab_key
 
 # Clone Git Library using Private Key from OCI Secrets Service
 
+echo "---Cloning SP3 Git"
 GIT_SSH_COMMAND='ssh -i /home/ubuntu/.ssh/gitlab_key -o StrictHostKeyChecking=no' git clone git@gitlab.com:MMMCloudPipeline/sp3.git
 
 # Create key pair for SSH to self
@@ -22,10 +23,12 @@ ssh-keygen -t rsa -f /home/ubuntu/.ssh/self_id_rsa -q -P ""
 cat /home/ubuntu/.ssh/self_id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 
 # Run first sp3 install script
+echo "---Running /home/ubuntu/sp3/sp3doc/install-basic.bash"
 ssh -i /home/ubuntu/.ssh/self_id_rsa -o StrictHostKeyChecking=no ubuntu@localhost bash /home/ubuntu/sp3/sp3doc/install-basic.bash
+echo "---Finished /home/ubuntu/sp3/sp3doc/install-basic.bash"
 
 # Get data from Object Storage
-
+echo "---Downloading data from object storage"
 sudo mkdir -p /data/inputs/uploads/oxforduni/
 
 oci os object get -bn artic_images --name artic-ncov2019-illumina.sif --file /tmp/artic-ncov2019-illumina.sif --auth instance_principal
@@ -38,11 +41,12 @@ sudo mv /tmp/*.sif /data/images/
 sudo chown root:root /data/images/*.sif
 
 # Extract sample data
-
+echo "---Extracting sample data"
 sudo tar -xf /tmp/210204_M01746_0015_000000000-JHB5M.tar --directory /data/inputs/uploads/oxforduni/
 rm /tmp/210204_M01746_0015_000000000-JHB5M.tar
 
 # Run second sp3 install script
+echo "---Running /home/ubuntu/sp3/sp3doc/install-oci.sh"
 ssh -i /home/ubuntu/.ssh/self_id_rsa -o StrictHostKeyChecking=no ubuntu@localhost bash /home/ubuntu/sp3/sp3doc/install-oci.sh
-
+echo "---Finished /home/ubuntu/sp3/sp3doc/install-oci.sh"
 
