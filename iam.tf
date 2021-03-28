49eab7f6b40ae4dae9d15f20090926c54a21287d
bhaworth <ben@benhaworth.com>
2021-03-28 11:38:07 +0100

    head node policy change to local compartment only

--- iam.tf
+++ iam.tf
@@ -7,14 +7,14 @@ resource "oci_identity_dynamic_group" "HeadNode_DG" {
 }
 
 resource "oci_identity_policy" "HeadNode_Comp_Policy" {
-  compartment_id = var.compartment_ocid
+  compartment_id = local.Sp3_cid
 
   description = "Policy for Head Node in deployment ${local.Sp3_env_name}"
 
   # Need to know what the correct permissions required are  <<CHANGE_ME>>
 
   statements = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.HeadNode_DG.name} to manage all-resources in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.HeadNode_DG.name} to manage all-resources in compartment id ${local.Sp3_cid}",
   ]
   name = "${local.Sp3_env_name}_HeadNode_Comp"
 }
