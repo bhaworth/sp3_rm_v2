30424b9ab6c093ea3cd2e750dae9c4835a394092
bhaworth <ben@benhaworth.com>
2021-04-13 10:22:00 +0100

    deploy 1000 sample option

--- variables.tf
+++ variables.tf
@@ -19,6 +19,7 @@ variable "ad" { default = "" }
 variable "name_prefix" { default = "" }
 variable "env_name" { default = "sp3" }
 variable "deploy_test" { default = false }
+variable "deploy_1k" { default = false }
 variable "show_testing_others" { default = false }
 variable "specify_prefix" { default = false }
 variable "worker_shape" { default = "" }
