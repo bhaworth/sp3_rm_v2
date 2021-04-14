3d89c32835599925c5bd5d256029e636566aca66
bhaworth <ben@benhaworth.com>
2021-04-14 19:54:26 +0100

    missing #!/bin/bash in 1000 samples script

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -62,6 +62,8 @@ oci os object get -bn artic_images --name artic-ncov2019-nanopore.sif --file /tm
 # Deploy script to download and extract 1000 samples post build, if required (user initiated)
 
 cat << EOF | tee -a /home/ubuntu/deploy_1k_samples.sh
+#!/bin/bash
+
 # Use this script to download and extract the 1000 samples stored in Object Storage
 # if you did not do this during the initial head node build process
 
