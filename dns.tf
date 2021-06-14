e5d19a0c41d9cbf92aa3a984bfd4dfaa06483f47
bhaworth <ben@benhaworth.com>
2021-06-14 16:00:30 +0100

    Change DNS to dev.gpas.world

--- dns.tf
+++ dns.tf
@@ -1,11 +1,11 @@
 resource "oci_dns_rrset" "lb_a_record" {
   count = var.create_dns ? 1 : 0
 
-  domain          = "${local.Sp3_env_name}.oci.sp3dev.ml"
+  domain          = "${local.Sp3_env_name}.${local.Sp3_dns_suffix}"
   rtype           = "A"
   zone_name_or_id = local.Sp3dev_ml_dns_zone_id
   items {
-    domain = "${local.Sp3_env_name}.oci.sp3dev.ml"
+    domain = "${local.Sp3_env_name}.${local.Sp3_dns_suffix}"
     rtype  = "A"
     rdata  = local.Sp3_lb_public_ip
     ttl    = 30
@@ -17,11 +17,11 @@ resource "oci_dns_rrset" "lb_a_record" {
 resource "oci_dns_rrset" "bastion_a_record" {
   count = var.create_dns ? 1 : 0
 
-  domain          = "bastion.${local.Sp3_env_name}.oci.sp3dev.ml"
+  domain          = "bastion.${local.Sp3_env_name}.${local.Sp3_dns_suffix}"
   rtype           = "A"
   zone_name_or_id = local.Sp3dev_ml_dns_zone_id
   items {
-    domain = "bastion.${local.Sp3_env_name}.oci.sp3dev.ml"
+    domain = "bastion.${local.Sp3_env_name}.${local.Sp3_dns_suffix}"
     rtype  = "A"
     rdata  = local.Sp3Bastion_public_ip
     ttl    = 30
