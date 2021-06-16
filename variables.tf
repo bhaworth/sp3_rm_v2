9a05c13f35d6619a008fbde6532977c039d46055
bhaworth <ben@benhaworth.com>
2021-06-16 10:42:45 +0100

    worker timeout and max co-creation capture

--- variables.tf
+++ variables.tf
@@ -29,6 +29,8 @@ variable "worker_ocpus" { default = 1 }
 variable "worker_ram" { default = 16 }
 variable "worker_min" { default = 1 }
 variable "worker_max" { default = 1 }
+variable "worker_timeout" { default = 30 }
+variable "worker_max_create" { default = 4 }
 variable "worker_use_scratch" { default = false }
 variable "create_child_comp" { default = true }
 variable "install_certs" { default = true }
@@ -37,12 +39,12 @@ variable "custom_worker_img" { default = "" }
 variable "select_cust_worker_img" { default = false }
 
 locals {
-  compute_flexible_shapes  = ["VM.Standard.E3.Flex"]
-  Sp3_deploy_id            = random_string.deploy_id.result
-  Sp3_gitrepo_secret_id    = "ocid1.vaultsecret.oc1.uk-london-1.amaaaaaahe4ejdia3ejrsbqkv6iz2ipwngjmteeduitufuu7u35sgxrx7wna"
+  compute_flexible_shapes   = ["VM.Standard.E3.Flex"]
+  Sp3_deploy_id             = random_string.deploy_id.result
+  Sp3_gitrepo_secret_id     = "ocid1.vaultsecret.oc1.uk-london-1.amaaaaaahe4ejdia3ejrsbqkv6iz2ipwngjmteeduitufuu7u35sgxrx7wna"
   Gpas_world_ssl_secret_id  = "ocid1.vaultsecret.oc1.uk-london-1.amaaaaaahe4ejdiaepnu52uznkweta4bkiqqdptincfh3zw5uubmfgxynxdq"
   Gpas_world_priv_secret_id = "ocid1.vaultsecret.oc1.uk-london-1.amaaaaaahe4ejdiap77sz5c6bx4eiremliirileqdpzs4neizp5buxprsu3a"
   Gpas_world_dns_zone_id    = "ocid1.dns-zone.oc1..0543d2fb4ca04efebe6d9f2d97e63c9b"
-  Sp3dev_sandbox_cid       = "ocid1.compartment.oc1..aaaaaaaao4kpjckz2pjmlict2ssrnx45ims7ttvxghlluo2tcwv6pgfdlepq"
-  Sp3_dns_suffix           = "dev.gpas.world"
+  Sp3dev_sandbox_cid        = "ocid1.compartment.oc1..aaaaaaaao4kpjckz2pjmlict2ssrnx45ims7ttvxghlluo2tcwv6pgfdlepq"
+  Sp3_dns_suffix            = "dev.gpas.world"
 }
