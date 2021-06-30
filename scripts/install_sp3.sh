c33e17ec871b963330d7d9fdbcedf5335a6aae83
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2021-06-30 12:48:11 +0100

    Escaping terraform variables

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -29,15 +29,15 @@ echo "---Finished /home/ubuntu/sp3/sp3doc/install-basic.bash"
 
 # Get pipelines from Object Storage
 echo "---Downloading pipelines from object storage"
-COVID_ENV_VERSION=$(curl -s -L -I -o /dev/null -w '%{url_effective}' https://github.com/GenomePathogenAnalysisService/SARS-COV-2_environments/releases/latest | xargs basename)
-oci os object bulk-download -bn artic_images --download-dir /tmp --overwrite --auth instance_principal --prefix ${COVID_ENV_VERSION}
+COVID_ENV_VERSION=$(curl -s -L -I -o /dev/null -w '%%{url_effective}' https://github.com/GenomePathogenAnalysisService/SARS-COV-2_environments/releases/latest | xargs basename)
+oci os object bulk-download -bn artic_images --download-dir /tmp --overwrite --auth instance_principal --prefix $${COVID_ENV_VERSION}
 VIRIDIAN_VERSION=$(basename /tmp/viridian* .img | rev | cut -d '_' -f 1 | rev )
 cat << EOF >> /home/ubuntu/.bashrc
 # Set what version of the COVID pipeline environment we are using
-export COVID_ENV_VERSION=${COVID_ENV_VERSION}
+export COVID_ENV_VERSION=$${COVID_ENV_VERSION}
 
 # Set what version of the viridian pipeline we are using
-export VIRIDIAN_VERSION=${VIRIDIAN_VERSION}
+export VIRIDIAN_VERSION=$${VIRIDIAN_VERSION}
 EOF
 
 # Move pipeline images to /data
