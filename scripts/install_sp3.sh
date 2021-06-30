1bac0d9b585efba1c8a1bf9b7528544034706c60
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2021-06-30 12:10:35 +0100

    Finding viridian version differently

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -31,8 +31,7 @@ echo "---Finished /home/ubuntu/sp3/sp3doc/install-basic.bash"
 echo "---Downloading pipelines from object storage"
 COVID_ENV_VERSION=$(curl -s -L -I -o /dev/null -w '%{url_effective}' https://github.com/GenomePathogenAnalysisService/SARS-COV-2_environments/releases/latest | xargs basename)
 oci os object bulk-download -bn artic_images --download-dir /tmp --overwrite --auth instance_principal --prefix ${COVID_ENV_VERSION}
-VIRIDIAN_VERSION=$(basename /tmp/viridian* .img)
-VIRIDIAN_VERSION=${VIRIDIAN_VERSION##*_}
+VIRIDIAN_VERSION=$(basename /tmp/viridian* .img | rev | cut -d '_' -f 1 | rev )
 cat << EOF >> /home/ubuntu/.bashrc
 # Set what version of the COVID pipeline environment we are using
 export COVID_ENV_VERSION=${COVID_ENV_VERSION}
