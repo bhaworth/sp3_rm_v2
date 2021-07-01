84a0905018225e0eaf23fb289529f4a58d48f59a
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2021-07-01 09:22:16 +0100

    Setting version variables in a json for reading in install scripts

--- scripts/install_sp3.sh
+++ scripts/install_sp3.sh
@@ -33,12 +33,12 @@ oci os object bulk-download -bn artic_images --download-dir /tmp --overwrite --a
 COVID_ENV_VERSION=$(curl -s -L -I -o /dev/null -w '%%{url_effective}' https://github.com/GenomePathogenAnalysisService/SARS-COV-2_environments/releases/latest | xargs basename)
 oci os object bulk-download -bn artic_images --download-dir /tmp --overwrite --auth instance_principal --prefix $${COVID_ENV_VERSION}
 VIRIDIAN_VERSION=$(basename /tmp/$${COVID_ENV_VERSION}/viridian* .img | rev | cut -d '_' -f 1 | rev)
-cat << EOF >> /home/ubuntu/.bashrc
-# Set what version of the COVID pipeline environment we are using
-export COVID_ENV_VERSION=$${COVID_ENV_VERSION}
-
-# Set what version of the viridian pipeline we are using
-export VIRIDIAN_VERSION=$${VIRIDIAN_VERSION}
+# Set variables for sp3 versions for bash install scripts
+cat << EOF >> /home/ubuntu/sp3_versions.json
+{ 
+    "covid_env_version":"$${COVID_ENV_VERSION}",
+    "viridian_version":"$${VIRIDIAN_VERSION}"
+}
 EOF
 
 # Move pipeline images to /data
